b1b1ae009649930eff6bc87e0a503ce53345125a
GennadiyPob <137322270+GennadiyPob@users.noreply.github.com>
2023-07-09 22:09:29 +0300

    Add calculator tool

---
+++ ex_15.py
@@ -0,0 +1,54 @@
+result = 0
+operand = None
+operator = None
+wait_for_number = True
+temp = 0
+
+# Вводимо першу змінну. Перевіряємо що введено. 
+# Якщо число - вихід з циклу. Інший символ, вводимо ще раз.
+
+while wait_for_number:
+    operand_1 = input()
+    try:
+        result = int(operand_1)
+    except ValueError:
+        print(f'{operand_1} is not number. Try again...')
+        continue
+    wait_for_number = False
+
+ #Перевірка чи не введено "=" - це закінченя розрахунків.
+while operator != '=':
+
+    if wait_for_number == False:
+        operator = input()
+        if operator not in {'+', '-', '*', '/'}:
+            print(f'{operator}) is not +, -, *, / or =. Try again...')
+            continue
+        wait_for_number = True
+        if operator == '=':
+            break
+    if wait_for_number:
+        operand_1 = input()
+        try:
+            operand = int(operand_1)
+        except ValueError:
+            print(f'{operand_1} is not number. Try again...')
+            continue
+        wait_for_number = False
+
+#Блок розрахунків
+
+    if operator == '+':
+        result += operand
+         
+    elif operator == '-':
+        result -= operand
+    
+    elif operator == '*':
+        result *= operand
+        
+    elif operator == '/':
+        result = result / operand
+
+
+print(f' Result : {result}')
